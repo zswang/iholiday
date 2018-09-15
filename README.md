6d0557294f77599ccb8225a8214b513438759964
zswang <wangjihu@sensorsdata.cn>
2018-09-15 14:49:55 +0800

    📝 Update @1.0.0

---
+++ README.md
@@ -0,0 +1,15 @@
+iholiday
+-------
+
+China holiday data
+
+中国节假日数据
+
+# [![NPM version][npm-image]][npm-url]
+
+## License
+
+MIT © [zswang](http://weibo.com/zswang)
+
+[npm-url]: https://npmjs.org/package/jfetchs-redis
+[npm-image]: https://badge.fury.io/js/jfetchs-redis.svg
--- scripts/spider.ts
+++ scripts/spider.ts
@@ -3,8 +3,19 @@
 import * as request from 'request'
 import * as iconv from 'iconv-lite'
 import * as moment from 'moment'
+import * as fs from 'fs'
+import * as yaml from 'js-yaml'
 import { Promise } from 'es6-promise'
 
+interface IHoliday {
+  festival: string
+  list: {
+    date: string
+    status: '1' | '2'
+  }[]
+  name: string
+  desc: string
+}
 interface ISpiderData {
   status: '0' | string
   data: {
@@ -12,21 +23,13 @@ interface ISpiderData {
       startday: string
       name: string
     }[]
-    holiday: {
-      festival: string
-      list: {
-        date: string
-        status: '1' | '2'
-      }[]
-      name: string
-      desc: string
-    }[]
+    holiday: IHoliday[] | IHoliday
   }[]
 }
 
 new Promise((resolve, reject) => {
   const startyear = 2011
-  const endyear = moment().year()
+  const endyear = moment().year() + 1
   let year = startyear - 1
   let result: { [startday: string]: string } = {}
   const next = () => {
@@ -35,6 +38,7 @@ new Promise((resolve, reject) => {
       return
     }
 
+    // #region request
     request(
       `https://sp0.baidu.com/8aQDcjqpAAV3otqbppnN2DJv/api.php?query=${encodeURIComponent(
         `${year}年1月`
@@ -58,11 +62,86 @@ new Promise((resolve, reject) => {
         next()
       }
     )
+    // #endregion
   }
   next()
 })
   .then(reply => {
-    console.log(reply)
+    const startdatas = Object.keys(reply)
+    let index = 0
+    return new Promise((resolve, reject) => {
+      const results = {}
+      const next = () => {
+        let startdata = startdatas[index++]
+        if (!startdata) {
+          resolve(results)
+          return
+        }
+        console.log(`${index}:${startdatas.length} ${startdata}`)
+        // #region request
+        request(
+          `https://sp0.baidu.com/8aQDcjqpAAV3otqbppnN2DJv/api.php?query=${encodeURIComponent(
+            `${moment(startdata, 'YYYY-M-D').format('YYYY年MM月DD日')}`
+          )}&resource_id=6018&ie=utf8&format=json`,
+          { encoding: null },
+          (err, _, body) => {
+            if (err) {
+              reject(err)
+              return
+            }
+            let reply = JSON.parse(iconv.decode(body, 'gbk')) as ISpiderData
+            if (reply.status !== '0') {
+              reject(reply.status)
+              return
+            }
+            if (reply.data[0] && reply.data[0].holiday) {
+              let holiday: IHoliday[]
+              if (!(reply.data[0].holiday instanceof Array)) {
+                holiday = [reply.data[0].holiday as IHoliday]
+              } else {
+                holiday = reply.data[0].holiday as IHoliday[]
+              }
+              holiday.forEach(item => {
+                item.list.forEach(day => {
+                  results[day.date.replace(/-(\d)\b/g, '-0$1')] = {
+                    note: `${item.name}・${{ 1: '休', 2: '班' }[day.status]}`,
+                    type: { 1: 'festival', 2: 'workday' }[day.status],
+                  }
+                })
+                results[item.festival.replace(/-(\d)\b/g, '-0$1')] = {
+                  note: `${item.name}`,
+                  type: 'festival',
+                }
+              })
+            }
+            setTimeout(() => {
+              next()
+            }, 1000)
+          }
+        )
+        // #endregion
+      }
+      next()
+    })
+  })
+  .then(reply => {
+    const keys = Object.keys(reply)
+    const days = {}
+    const years = []
+    keys.sort((a, b) => {
+      return a.localeCompare(b)
+    })
+    keys.forEach(key => {
+      days[key] = reply[key]
+      const y = key.slice(0, 4)
+      years[y] = years[y] || {}
+      years[y][key] = reply[key]
+    })
+
+    fs.writeFileSync(`./lib/days.json`, JSON.stringify(days, null, '  '))
+    Object.keys(years).forEach(key => {
+      fs.writeFileSync(`./year/${key}.yaml`, yaml.safeDump(years[key]))
+    })
   })
   .catch(err => {
     console.error(err)
